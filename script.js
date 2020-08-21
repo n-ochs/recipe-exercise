c5896de9e9b1c06e5b09fef569e2d44b036040c0
Nicholas Ochs <nick.ochs@yahoo.com>
2020-08-20 20:39:16 -0400

    Initial commit

---
+++ script.js
@@ -0,0 +1,15 @@
+// This is a recipe for marinara sauace
+
+let two = 2
+let oneFourth = "1/4"
+
+function marinaraSauce(cookName = "Adam") {
+    console.log("Hey, " + cookName + "! This is my marinara sauce recipe!");
+    console.log("Heat a medium-large saucepan over medium heat.");
+    console.log("Add " + two + " tspns of oil and 5 garlic cloves, cook until golden, about " + two +  " minutes");
+    console.log("Add " + oneFourth + " cup water, " + two + " cans of crushed tomatoes, and salt and season with black pepper to taste.");
+    console.log("Cover the pot, bring to a boil, reduce the heat to medium low, and simmer until the sauce is heated, about 10 minutes");
+    console.log("Stir in " + oneFourth + " cup of roughly chopped fresh basil, salt and pepper as needed.");
+}
+
+marinaraSauce()
